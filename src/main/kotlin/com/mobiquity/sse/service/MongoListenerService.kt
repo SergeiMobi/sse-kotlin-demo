7d663853a766ef867f450824a4004cdc1ecf6f97
Sergei Lebedev <slebedev@mobiquityinc.com>
2021-02-03 22:44:14 +0100

    add logs

--- src/main/kotlin/com/mobiquity/sse/service/MongoListenerService.kt
+++ src/main/kotlin/com/mobiquity/sse/service/MongoListenerService.kt
@@ -1,16 +1,19 @@
 package com.mobiquity.sse.service
 
 import com.mobiquity.sse.model.EventModel
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener
 import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent
 import org.springframework.stereotype.Component
 
-
 @Component
 class MongoListenerService(val publisher: Publisher) : AbstractMongoEventListener<EventModel?>() {
+
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
+
     override fun onAfterSave(event: AfterSaveEvent<EventModel?>) {
-        println("" + event.getSource())
-        println("" + event.getDocument())
+        log.debug("MongoDb listener service document: " + event.document)
         publisher.publish(EventModel(event.document.toString()))
     }
 }
--- src/main/kotlin/com/mobiquity/sse/service/Subscriber.kt
+++ src/main/kotlin/com/mobiquity/sse/service/Subscriber.kt
@@ -1,18 +1,25 @@
 package com.mobiquity.sse.service
 
 import com.mobiquity.sse.model.EventModel
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.config.ConfigurableBeanFactory
+import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Service
 import java.util.function.Consumer
 
 @Service
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 class Subscriber(pubSubService: PubSubService) {
-    var subscribe: Consumer<EventModel>? = null
+    val log: Logger = LoggerFactory.getLogger(this::class.java)
 
+    var subscribe: Consumer<EventModel>? = null
     init {
         pubSubService.registerSubscriber(this)
     }
 
     fun receivedMessage(pushEvent: EventModel) {
+        log.debug("subscribe event is received: $pushEvent")
             subscribe?.accept(pushEvent)
     }
 
